7df8656a2907f18aae4830b9b7a1a621016eeef7
RV <razvanvancea94@gmail.com>
2023-05-23 10:26:46 +0300

    Update main.ts

--- employee-api/src/main.ts
+++ employee-api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+const PORT = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -14,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
 
-  await app.listen(3001);
+  await app.listen(PORT);
 }
 bootstrap();
